7b9afab580ec6586e562b4e188c64d9630f3f573
issyzac <issyzac.iz@gmail.com>
2021-12-14 11:04:14 +0300

    Listen on click of the list items

--- opensrp-chw/src/main/java/org/smartregister/chw/fragment/AddMemberFragment.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/fragment/AddMemberFragment.java
@@ -6,9 +6,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import androidx.annotation.Nullable;
+
 import org.smartregister.chw.R;
 import org.smartregister.chw.core.activity.CoreFamilyProfileActivity;
 import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.util.Constants;
 
 import timber.log.Timber;
 
@@ -16,9 +19,12 @@ public class AddMemberFragment extends org.smartregister.chw.core.fragment.AddMe
 
     private Context context;
 
+    public static AddMemberFragment newInstance() {
+        return new AddMemberFragment();
+    }
+
     @Override
     public void setContext(Context context) {
-        super.setContext(context);
         this.context = context;
     }
 
@@ -28,14 +34,26 @@ public class AddMemberFragment extends org.smartregister.chw.core.fragment.AddMe
     }
 
     @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        view.findViewById(R.id.close).setOnClickListener(this);
+        view.findViewById(R.id.layout_add_anc).setOnClickListener(this);
+        view.findViewById(R.id.layout_add_caregiver).setOnClickListener(this);
+    }
+
+    @Override
     public void onClick(View v) {
-        super.onClick(v);
         try {
             int i = v.getId();
-            if (i == R.id.layout_add_anc) {
-                ((CoreFamilyProfileActivity) context).startChildForm(CoreConstants.JSON_FORM.getChildRegister(), "", "", "");
+            if (i == R.id.close){
+                dismiss();
+            }
+            else if (i == R.id.layout_add_anc) {
+                //TODO: Fire up anc reg form
+                ((CoreFamilyProfileActivity) context).startChildForm(Constants.JSON_FORM_CONSTANT.getAncMotherRegistration(), "", "", "");
                 dismiss();
             } else if (i == R.id.layout_add_caregiver) {
+                //TODO: Fire up care giver registration form
                 ((CoreFamilyProfileActivity) context).startFormActivity(CoreConstants.JSON_FORM.getFamilyMemberRegister(), null, "");
                 dismiss();
             }
