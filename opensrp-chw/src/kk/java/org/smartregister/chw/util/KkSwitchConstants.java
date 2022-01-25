05aaaf43261471ae3c91f414c67ba7630079bfb3
Kassim Sheghembe <sheghembekassim@gmail.com>
2022-01-25 13:49:30 +0300

    KS | Implement; environment switching activity with a fragment to clear environment data before switching to a new one.

--- opensrp-chw/src/kk/java/org/smartregister/chw/util/KkSwitchConstants.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/util/KkSwitchConstants.java
@@ -4,4 +4,9 @@ package org.smartregister.chw.util;
  * Created by Kassim Sheghembe on 2022-01-24
  */
 public class KkSwitchConstants {
+
+    public static final String KIZAZI_ENVIRONMENT = "opensrp_kizazi_environment";
+    public static final String IS_PRODUCTION_ENABLED = "enable_production";
+    public static final String PRODUCTION_ENV = "Production";
+    public static final String TEST_ENV = "Test";
 }
