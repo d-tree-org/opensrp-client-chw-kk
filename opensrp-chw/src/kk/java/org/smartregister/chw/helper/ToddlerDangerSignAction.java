a879d6c1d1dcf4ec114c76d55e03a1245b46e319
issyzac <issyzac.iz@gmail.com>
2022-03-02 18:07:44 +0300

    :zap: Todder dager sign action definition

--- opensrp-chw/src/kk/java/org/smartregister/chw/helper/ToddlerDangerSignAction.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/helper/ToddlerDangerSignAction.java
@@ -1,4 +1,69 @@
 package org.smartregister.chw.helper;
 
-public class ToddlerDangerSignAction {
+import android.content.Context;
+
+import org.apache.commons.lang3.StringUtils;
+import org.joda.time.LocalDate;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.smartregister.chw.R;
+import org.smartregister.chw.anc.actionhelper.HomeVisitActionHelper;
+import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
+import org.smartregister.chw.util.JsonFormUtils;
+import org.smartregister.domain.Alert;
+
+import timber.log.Timber;
+
+public class ToddlerDangerSignAction extends HomeVisitActionHelper {
+
+    private Context context;
+    private String toddler_danger_sign;
+    private Alert alert;
+
+    public ToddlerDangerSignAction(Context context, Alert alert){
+        this.context = context;
+        this.alert = alert;
+    }
+
+    @Override
+    public BaseAncHomeVisitAction.ScheduleStatus getPreProcessedStatus() {
+        return isOverDue() ? BaseAncHomeVisitAction.ScheduleStatus.OVERDUE : BaseAncHomeVisitAction.ScheduleStatus.DUE;
+    }
+
+    private boolean isOverDue() {
+        return new LocalDate().isAfter(new LocalDate(alert.startDate()).plusDays(14));
+    }
+
+    @Override
+    public void onPayloadReceived(String jsonPayload) {
+        try {
+            JSONObject jsonObject = new JSONObject(jsonPayload);
+            toddler_danger_sign  = JsonFormUtils.getValue(jsonObject, "toddler_danger_sign");
+        }catch (JSONException e){
+            Timber.e(e);
+        }
+    }
+
+    @Override
+    public String evaluateSubTitle() {
+        if (StringUtils.isBlank(toddler_danger_sign))
+            return "";
+
+        //Check if there are danger signs for the tolder
+        return "No".equalsIgnoreCase(toddler_danger_sign) ? context.getString(R.string.yes) : context.getString(R.string.no);
+    }
+
+    @Override
+    public BaseAncHomeVisitAction.Status evaluateStatusOnPayload() {
+        if (StringUtils.isBlank(toddler_danger_sign))
+            return BaseAncHomeVisitAction.Status.PENDING;
+
+        if (toddler_danger_sign.equalsIgnoreCase("Yes")) {
+            return BaseAncHomeVisitAction.Status.PARTIALLY_COMPLETED;
+        } else if (toddler_danger_sign.equalsIgnoreCase("No")) {
+            return BaseAncHomeVisitAction.Status.COMPLETED;
+        } else {
+            return BaseAncHomeVisitAction.Status.PENDING;
+        }
+    }
 }
