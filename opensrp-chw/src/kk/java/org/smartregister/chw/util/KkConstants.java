47cb31afb1bcc73b03783770f6913c283f00a3a1
Kassim Sheghembe <sheghembekassim@gmail.com>
2022-03-03 12:16:13 +0300

    Added a class for Constants.

--- opensrp-chw/src/kk/java/org/smartregister/chw/util/KkConstants.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/util/KkConstants.java
@@ -1,5 +1,22 @@
-package org.smartregister.chw.util;/**
+package org.smartregister.chw.util;
+
+/**
  * Created by Kassim Sheghembe on 2022-02-17
- * 
- */public class KkConstants {
+ */
+public class KkConstants extends Constants{
+
+    public static class KKJSON_FORM_CONSTANT {
+
+        public static class KKCHILD_HOME_VISIT {
+
+            public static final String CHILD_HV_BREASTFEEDING = "child_hv_breastfeeding";
+
+            public static String getChildHvBreastfeeding() {
+                return Utils.getLocalForm(CHILD_HV_BREASTFEEDING);
+            }
+
+        }
+
+    }
+
 }
