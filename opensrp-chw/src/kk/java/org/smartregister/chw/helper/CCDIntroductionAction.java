97901a4aeeeb4169c5b5ec3834950b76c17ba42f
issyzac <issyzac.iz@gmail.com>
2022-03-03 15:15:58 +0300

    :new: CCD Itroduction topic action helper

---
+++ opensrp-chw/src/kk/java/org/smartregister/chw/helper/CCDIntroductionAction.java
@@ -0,0 +1,49 @@
+package org.smartregister.chw.helper;
+
+import org.joda.time.LocalDate;
+import org.json.JSONObject;
+import org.smartregister.Context;
+import org.smartregister.chw.anc.actionhelper.HomeVisitActionHelper;
+import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
+import org.smartregister.domain.Alert;
+
+import timber.log.Timber;
+
+public class CCDIntroductionAction extends HomeVisitActionHelper {
+
+    private final Context context;
+    private final Alert alert;
+
+    public CCDIntroductionAction(Context mContext, Alert mAlert){
+        this.alert = mAlert;
+        this.context = mContext;
+    }
+
+    @Override
+    public BaseAncHomeVisitAction.ScheduleStatus getPreProcessedStatus() {
+        return isOverDue() ? BaseAncHomeVisitAction.ScheduleStatus.OVERDUE : BaseAncHomeVisitAction.ScheduleStatus.DUE;
+    }
+
+    private boolean isOverDue(){
+        return new LocalDate().isAfter(new LocalDate(alert.startDate()).plusDays(14));
+    }
+
+    @Override
+    public void onPayloadReceived(String jsonPayload) {
+        try{
+            JSONObject jsonObject = new JSONObject(jsonPayload);
+        }catch (Exception e){
+            Timber.e(e);
+        }
+    }
+
+    @Override
+    public String evaluateSubTitle() {
+        return null;
+    }
+
+    @Override
+    public BaseAncHomeVisitAction.Status evaluateStatusOnPayload() {
+        return null;
+    }
+}
