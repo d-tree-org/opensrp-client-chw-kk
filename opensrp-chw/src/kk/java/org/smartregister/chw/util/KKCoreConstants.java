01d1ea498a7e40c4f054ffd75a1f7cceca1d1c8c
issyzac <issyzac.iz@gmail.com>
2022-03-25 15:27:36 +0300

    :zap: include new topic event name

--- opensrp-chw/src/kk/java/org/smartregister/chw/util/KKCoreConstants.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/util/KKCoreConstants.java
@@ -20,5 +20,6 @@ public class KKCoreConstants extends CoreConstants {
         public static final String CCD_DEVELOPMENT_SCREENING = "Child Home Visit - CCD Development Screening";
         public static final String CCD_CHILD_DISCIPLINE = "Child Home Visit - CCD Child Discipline";
         public static final String CHILD_SAFETY = "Child Home Visit - Child Safety";
+        public static final String COMPLIMENTARY_FEEDING = "Child Home Visit - Complimentary Feeding";
     }
 }
