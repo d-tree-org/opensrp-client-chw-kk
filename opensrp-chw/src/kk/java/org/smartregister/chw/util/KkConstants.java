4fe3f3ee4861071d58e18d84149253091046c0f7
issyzac <issyzac.iz@gmail.com>
2022-04-30 23:31:54 +0300

    :recycle: form name constant

--- opensrp-chw/src/kk/java/org/smartregister/chw/util/KkConstants.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/util/KkConstants.java
@@ -51,6 +51,7 @@ public class KkConstants extends Constants{
             public static final String PNC_HV_MATERNAL_NUTRITION = "pnc_hv_maternal_nutrition";
             public static final String PNC_HV_HIV_AIDS_GENERAL_INFO = "pnc_hv_hiv_aids_general_info";
             public static final String PNC_HV_LAM = "pnc_hv_lam";
+            public static final String PNC_MOTHER_CARE = "pnc_hv_postpartum_care_for_mother";
 
             public static String getPncHvDangerSigns() {
                 return Utils.getLocalForm(PNC_HV_DANGER_SIGNS);
@@ -66,6 +67,10 @@ public class KkConstants extends Constants{
 
             public static String getPncHvLam() {
                 return Utils.getLocalForm(PNC_HV_LAM);
+            }
+
+            public static String getPncMotherCare(){
+                return Utils.getLocalForm(PNC_MOTHER_CARE);
             }
 
         }
