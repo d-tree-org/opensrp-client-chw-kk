21190eae32840a74f102a077ba94178e094b71b4
Allan <a.onchuru@gmail.com>
2022-05-18 11:14:42 +0300

    :construction: Add create referral from ANC HV danger-signs

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/ChildHomeVisitActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/ChildHomeVisitActivity.java
@@ -1,9 +1,24 @@
 package org.smartregister.chw.activity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
 import org.smartregister.chw.anc.presenter.BaseAncHomeVisitPresenter;
+import org.smartregister.chw.application.ChwApplication;
 import org.smartregister.chw.core.activity.CoreChildHomeVisitActivity;
 import org.smartregister.chw.core.interactor.CoreChildHomeVisitInteractor;
+import org.smartregister.chw.core.utils.CoreConstants;
+import org.smartregister.chw.core.utils.CoreReferralUtils;
 import org.smartregister.chw.interactor.ChildHomeVisitInteractorFlv;
+import org.smartregister.chw.schedulers.ChwScheduleTaskExecutor;
+
+import java.util.Date;
+
+import timber.log.Timber;
 
 public class ChildHomeVisitActivity extends CoreChildHomeVisitActivity {
     @Override
@@ -17,4 +32,64 @@ public class ChildHomeVisitActivity extends CoreChildHomeVisitActivity {
         ChildProfileActivity.startMe(this, memberObject, ChildProfileActivity.class);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == org.smartregister.chw.anc.util.Constants.REQUEST_CODE_GET_JSON) {
+            if (resultCode == Activity.RESULT_OK) {
+                try {
+                    String jsonString = data.getStringExtra(org.smartregister.chw.anc.util.Constants.JSON_FORM_EXTRA.JSON);
+
+                    //check if client is being referred
+                    JSONObject form = new JSONObject(jsonString);
+                    JSONArray a = form.getJSONObject("step1").getJSONArray("fields");
+                    String buttonAction = "";
+
+                    for (int i=0; i<a.length(); i++) {
+                        org.json.JSONObject jo = a.getJSONObject(i);
+                        if (jo.getString("key").compareToIgnoreCase("save_n_link") == 0 || jo.getString("key").compareToIgnoreCase("save_n_refer") == 0) {
+                            if(jo.optString("value") != null && jo.optString("value").compareToIgnoreCase("true") == 0){
+                                buttonAction = jo.getJSONObject("action").getString("behaviour");
+                            }
+                        }
+                    }
+                    if(!buttonAction.isEmpty()) {
+                        // check if other referral exists
+                        String entityID = baseEntityID != null ? baseEntityID : memberObject.getBaseEntityId();
+                        String businessStatus = buttonAction.equalsIgnoreCase("refer") ? CoreConstants.BUSINESS_STATUS.REFERRED : CoreConstants.BUSINESS_STATUS.LINKED;
+                        if (!CoreReferralUtils.hasReferralTask(entityID, businessStatus)) {
+                            //refer
+                            CoreReferralUtils.createReferralEvent(ChwApplication.getInstance().getContext().allSharedPreferences(),
+                                    jsonString, CoreConstants.TABLE_NAME.CHILD_REFERRAL, entityID);
+
+                            //add referral schedule
+                            ChwScheduleTaskExecutor.getInstance().execute(memberObject.getBaseEntityId(), CoreConstants.EventType.CHILD_REFERRAL, new Date());
+
+                            Toast.makeText(getContext(), getResources().getString(org.smartregister.chw.R.string.referral_submitted), Toast.LENGTH_LONG).show();
+                        }
+                    }
+
+                    //end of check referral
+                    BaseAncHomeVisitAction ancHomeVisitAction = actionList.get(current_action);
+                    if (ancHomeVisitAction != null) {
+                        ancHomeVisitAction.setJsonPayload(jsonString);
+                    }
+                } catch (Exception e) {
+                    Timber.e(e);
+                    Toast.makeText(this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                }
+            } else {
+
+                BaseAncHomeVisitAction ancHomeVisitAction = actionList.get(current_action);
+                if (ancHomeVisitAction != null)
+                    ancHomeVisitAction.evaluateStatus();
+            }
+
+        }
+
+        // update the adapter after every payload
+        if (mAdapter != null) {
+            mAdapter.notifyDataSetChanged();
+            redrawVisitUI();
+        }
+    }
 }
