9458d821892232f0a51dc137981b25be13045e4f
issyzac <issyzac.iz@gmail.com>
2022-06-09 16:23:54 +0300

    :construction: link referall followup from pnc register

--- opensrp-chw/src/main/java/org/smartregister/chw/provider/PncRegisterProvider.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/provider/PncRegisterProvider.java
@@ -6,12 +6,17 @@ import android.view.View;
 import android.widget.Button;
 
 import org.smartregister.chw.R;
+import org.smartregister.chw.activity.AncRegisterActivity;
+import org.smartregister.chw.activity.PncRegisterActivity;
+import org.smartregister.chw.activity.ReferralFollowupActivity;
 import org.smartregister.chw.anc.provider.AncRegisterProvider;
 import org.smartregister.chw.core.provider.ChwPncRegisterProvider;
 import org.smartregister.chw.core.rule.PncVisitAlertRule;
+import org.smartregister.chw.core.utils.CoreReferralUtils;
 import org.smartregister.chw.core.utils.VisitSummary;
 import org.smartregister.chw.util.TaskUtils;
 import org.smartregister.commonregistry.CommonRepository;
+import org.smartregister.domain.Task;
 import org.smartregister.view.contract.SmartRegisterClient;
 
 import java.util.Set;
@@ -40,16 +45,25 @@ public class PncRegisterProvider extends ChwPncRegisterProvider {
     protected void updateDueColumn(Context context, RegisterViewHolder viewHolder, PncVisitAlertRule pncVisitAlertRule, String baseEntityId) {
         super.updateDueColumn(context, viewHolder, pncVisitAlertRule, baseEntityId);
         viewHolder.dueButton.setVisibility(View.VISIBLE);
-        if (TaskUtils.hasReferralDue(TaskUtils.getClientReferralTasks(baseEntityId))) {
-            setVisitButtonDueStatus(context, viewHolder.dueButton);
+
+        //Check if user has a referral task open
+        Task task = CoreReferralUtils.getTaskForEntity(baseEntityId, true);
+        if (task != null && task.getStatus() == Task.TaskStatus.READY){
+            setReferralDueButton(context, viewHolder.dueButton, baseEntityId);
         }
+
     }
 
-    private void setVisitButtonDueStatus(Context context, Button dueButton) {
+    private void setReferralDueButton(Context context, Button dueButton, String baseEntityId) {
         dueButton.setTextColor(context.getResources().getColor(R.color.white));
         dueButton.setText(context.getString(R.string.referral_followup));
         dueButton.setBackgroundResource(R.drawable.overdue_red_btn_selector);
-        dueButton.setOnClickListener(onClickListener);
+        dueButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ReferralFollowupActivity.startReferralFollowupActivity((PncRegisterActivity) context, CoreReferralUtils.getTaskForEntity(baseEntityId, true).getIdentifier(), baseEntityId);
+            }
+        });
     }
 
 
