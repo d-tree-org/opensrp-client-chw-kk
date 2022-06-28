b2b222bc36eb4e5601ed8724b8c9a573c3cec03d
Kassim Sheghembe <sheghembekassim@gmail.com>
2022-06-28 12:03:54 -0400

    :bug: Fix the serviceIteration and visit number for double digits visit number and service iterations.

--- opensrp-chw/src/kk/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
@@ -47,6 +47,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 import timber.log.Timber;
 
@@ -83,7 +85,7 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
                 ServiceWrapper communicationAssessmentServiceWrapper = serviceWrapperMap.get("CCD communication assessment");
                 if (communicationAssessmentServiceWrapper != null) {
                     String communicationAssessmentModuleName = communicationAssessmentServiceWrapper.getName();
-                    visitNumber = communicationAssessmentModuleName.substring(communicationAssessmentModuleName.length() - 1);
+                    visitNumber = getVisitNumberFromServiceName(communicationAssessmentModuleName);
                 }
             }
         } catch (Exception e) {
@@ -327,7 +329,7 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
         Alert alert = serviceWrapper.getAlert();
         if (alert == null || new LocalDate().isBefore(new LocalDate(alert.startDate()))) return;
 
-        final String serviceIteration = serviceWrapper.getName().substring(serviceWrapper.getName().length() - 1);
+        final String serviceIteration = getVisitNumberFromServiceName(serviceWrapper.getName());
         String title = context.getString(R.string.ccd_introduction, serviceIteration);
 
         // alert if overdue after 14 days
@@ -360,7 +362,7 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
         Alert alert = serviceWrapper.getAlert();
         if (alert == null || new LocalDate().isBefore(new LocalDate(alert.startDate()))) return;
 
-        final String serviceIteration = serviceWrapper.getName().substring(serviceWrapper.getName().length() - 1);
+        final String serviceIteration = getVisitNumberFromServiceName(serviceWrapper.getName());
         String title = context.getString(R.string.ccd_development_screening, serviceIteration);
 
         // alert if overdue after 14 days
@@ -394,7 +396,7 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
         Alert alert = serviceWrapper.getAlert();
         if (alert == null || new LocalDate().isBefore(new LocalDate(alert.startDate()))) return;
 
-        final String serviceIteration = serviceWrapper.getName().substring(serviceWrapper.getName().length() - 1);
+        final String serviceIteration = getVisitNumberFromServiceName(serviceWrapper.getName());
         String title = context.getString(R.string.ccd_communication_assessment, serviceIteration);
 
         // alert if overdue after 14 days
@@ -496,7 +498,7 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
         Alert alert = serviceWrapper.getAlert();
         if (alert == null || new LocalDate().isBefore(new LocalDate(alert.startDate()))) return;
 
-        final String serviceIteration = serviceWrapper.getName().substring(serviceWrapper.getName().length() - 1);
+        final String serviceIteration = getVisitNumberFromServiceName(serviceWrapper.getName());
         String title = context.getString(R.string.complimentary_feeding, serviceIteration);
 
         // alert if overdue after 14 days
@@ -735,5 +737,16 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
             translatedText = context.getString(R.string.month_of, period);
         }
         return translatedText;
+    }
+
+    private String getVisitNumberFromServiceName(String serviceName) {
+        final Pattern lastIntPattern = Pattern.compile("[^0-9]+([0-9]+)$");
+        Matcher matcher = lastIntPattern.matcher(serviceName);
+        if (matcher.find()) {
+            String someNumberStr = matcher.group(1);
+            assert someNumberStr != null;
+            return someNumberStr;
+        }
+        return "0";
     }
 }
