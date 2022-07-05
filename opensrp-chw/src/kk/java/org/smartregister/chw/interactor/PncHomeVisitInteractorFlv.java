e46f40b4e9a3d8da39aa2f588cc3354a2f4278e2
Kassim Sheghembe <sheghembekassim@gmail.com>
2022-07-05 10:29:07 -0400

    :bug: Fix showing Postpartum Care for mother module in 1st, 2nd and 3rd Pnc Visits.

--- opensrp-chw/src/kk/java/org/smartregister/chw/interactor/PncHomeVisitInteractorFlv.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/interactor/PncHomeVisitInteractorFlv.java
@@ -212,8 +212,7 @@ public class PncHomeVisitInteractorFlv extends DefaultPncHomeVisitInteractorFlv 
 
             String visitID = visitID = visitSummary.getVisitID();
 
-            if (visitID == null || !visitID.equalsIgnoreCase("1") || !visitID.equalsIgnoreCase("3")
-                    || !visitID.equalsIgnoreCase("8")) return;
+            if (visitID == null || visitID.equalsIgnoreCase("21") || visitID.equalsIgnoreCase("35")) return;
 
             String title = context.getString(R.string.pnc_postpartum_mother_care);
 
