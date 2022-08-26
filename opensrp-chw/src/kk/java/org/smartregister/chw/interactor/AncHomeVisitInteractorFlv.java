7a18c5c0c27fa6f4af11e89b87ee16161ada736f
devcolonel <dev.marioanthony@gmail.com>
2022-08-26 12:25:39 +0300

    remove hardcoded messages

--- opensrp-chw/src/kk/java/org/smartregister/chw/interactor/AncHomeVisitInteractorFlv.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/interactor/AncHomeVisitInteractorFlv.java
@@ -631,7 +631,7 @@ public class AncHomeVisitInteractorFlv implements AncHomeVisitInteractor.Flavor 
 
         @Override
         public String evaluateSubTitle() {
-            return MessageFormat.format("{0}: {1}", "Number of clinic attendance", clinic_attendance );
+            return MessageFormat.format("{0}: {1}", context.getString(R.string.anc_hv_clinic_attendance_sub_title), clinic_attendance);
         }
 
         @Override
