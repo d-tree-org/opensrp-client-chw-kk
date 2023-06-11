22bdac2fc76575475478d8557f09cfddcbbfa25f
issyzac <issyzac.iz@gmail.com>
2023-06-11 23:55:04 +0300

    :zap: group session form and event fields

---
+++ opensrp-chw/src/kk/java/org/smartregister/chw/dao/GroupCurriculumDao.java
@@ -0,0 +1,12 @@
+package org.smartregister.chw.dao;
+
+import org.smartregister.dao.AbstractDao;
+
+/**
+ * Author issyzac on 11/06/2023
+ */
+public class GroupCurriculumDao extends AbstractDao {
+
+
+
+}
