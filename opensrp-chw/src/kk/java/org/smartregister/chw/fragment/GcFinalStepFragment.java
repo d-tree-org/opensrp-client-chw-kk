fdc5c0be93fc66f53b52d5961511233b552832f2
issyzac <issyzac.iz@gmail.com>
2023-07-04 20:18:13 +0300

    :art: add layout for gc final step

--- opensrp-chw/src/kk/java/org/smartregister/chw/fragment/GcFinalStepFragment.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/fragment/GcFinalStepFragment.java
@@ -1,7 +1,38 @@
 package org.smartregister.chw.fragment;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.Nullable;
+
 /**
  * Author issyzac on 04/07/2023
  */
-public class GcFinalStepFragment {
+public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment {
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        super.onCreateView(inflater, container, savedInstanceState);
+        View view = rootView;
+
+        return rootView;
+    }
+
+    @Override
+    public void setupViews(View view) {
+        super.setupViews(view);
+    }
+
+    @Override
+    protected int getLayout() {
+        return super.getLayout();
+    }
+
+    @Override
+    public String getSessionDetails() {
+        return null;
+    }
 }
