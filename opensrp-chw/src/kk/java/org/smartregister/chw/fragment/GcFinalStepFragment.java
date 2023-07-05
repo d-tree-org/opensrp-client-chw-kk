44b09cf4578fd388a1ceac80f2820a6504636f95
issyzac <issyzac.iz@gmail.com>
2023-07-05 16:56:00 +0300

    :zap: add checkbox controls

--- opensrp-chw/src/kk/java/org/smartregister/chw/fragment/GcFinalStepFragment.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/fragment/GcFinalStepFragment.java
@@ -14,7 +14,11 @@ import androidx.annotation.Nullable;
 import org.apache.commons.io.serialization.ValidatingObjectInputStream;
 import org.json.JSONObject;
 import org.smartregister.chw.R;
+import org.smartregister.chw.listener.CaregiversEncouragingListener;
+import org.smartregister.chw.listener.CaregiversMaterialsListener;
 import org.smartregister.chw.model.GroupSessionModel;
+
+import java.util.List;
 
 /**
  * Author issyzac on 04/07/2023
@@ -36,16 +40,35 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment {
     CheckBox caregiversBroughtMaterialsYesSome;
     CheckBox caregiversBroughtMaterialsNoneOrFew;
 
+    CheckBox activityWelcome;
+    CheckBox activityOpeningSong;
+    CheckBox activityReview;
+    CheckBox activityOne;
+    CheckBox activityTwo;
+    CheckBox activityRecap;
+    CheckBox activityClosingSong;
+
+    CheckBox freePlayMostChildren;
+    CheckBox freePlayOneAdult;
+
+    CheckBox difficultWelcome;
+    CheckBox difficultOpeningSong;
+    CheckBox difficultReview;
+    CheckBox difficultActivity1;
+    CheckBox difficultActivity2;
+    CheckBox difficultRecap;
+    CheckBox difficultClosingSong;
+
     CheckBox materialsScheduledUsedYes;
     CheckBox materialsScheduledUsedNo;
 
     GroupSessionModel sessionModel;
 
-    private String activitiesTookPlace;
+    private List<Activities> activitiesTookPlace;
     private boolean teachingLearningMaterialsUsed;
-    private String unguidedFreePlay;
+    private List<UnguidedFreePlay> unguidedFreePlay;
     private boolean anyDifficultActivities = false;
-    private String listOfDifficultActivities = "";
+    private List<Activities> listOfDifficultActivities;
     private String caregiversEncouraging;
     private String caregiversBroughtMaterials;
     private String topicsCovered;
@@ -73,13 +96,117 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment {
     public void setupViews(View view) {
         super.setupViews(view);
 
+        //List of difficult Activities
+        difficultWelcome = view.findViewById(R.id.difficult_welcome_and_free_play);
+        difficultWelcome.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    listOfDifficultActivities.add(Activities.WELCOME_AND_FREE_PLAY);
+                else
+                    listOfDifficultActivities.remove(Activities.WELCOME_AND_FREE_PLAY);
+            }
+        });
+
+        difficultOpeningSong = view.findViewById(R.id.difficult_opening_song);
+        difficultOpeningSong.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    listOfDifficultActivities.add(Activities.OPENING_SONG);
+                else
+                    listOfDifficultActivities.remove(Activities.OPENING_SONG);
+            }
+        });
+
+        difficultReview = view.findViewById(R.id.difficult_review_prevous_week);
+        difficultReview.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    listOfDifficultActivities.add(Activities.REVIEW_PREVIOUS_WEEK);
+                else
+                    listOfDifficultActivities.remove(Activities.REVIEW_PREVIOUS_WEEK);
+            }
+        });
+
+
+        difficultActivity1 = view.findViewById(R.id.difficult_activity_1);
+        difficultActivity1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    listOfDifficultActivities.add(Activities.ACTIVITY_1);
+                else
+                    listOfDifficultActivities.remove(Activities.ACTIVITY_1);
+            }
+        });
+
+        difficultActivity2 = view.findViewById(R.id.difficult_activity_2);
+        difficultActivity2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    listOfDifficultActivities.add(Activities.ACTIVITY_TWO);
+                else
+                    listOfDifficultActivities.remove(Activities.ACTIVITY_TWO);
+            }
+        });
+
+        difficultRecap = view.findViewById(R.id.difficult_recap_session);
+        difficultRecap.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    listOfDifficultActivities.add(Activities.RECAP_SESSION);
+                else
+                    listOfDifficultActivities.remove(Activities.RECAP_SESSION);
+            }
+        });
+
+        difficultClosingSong = view.findViewById(R.id.difficult_closing_song);
+        difficultClosingSong.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    listOfDifficultActivities.add(Activities.CLOSING_SONG);
+                else
+                    listOfDifficultActivities.remove(Activities.CLOSING_SONG);
+            }
+        });
+
+        //Free Play checkboxes
+        freePlayMostChildren = view.findViewById(R.id.free_play_most_children);
+        freePlayMostChildren.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    unguidedFreePlay.add(UnguidedFreePlay.MOST_CHILDREN_ARE_PLAYING_WITH_MATERIALS);
+                else
+                    unguidedFreePlay.remove(UnguidedFreePlay.MOST_CHILDREN_ARE_PLAYING_WITH_MATERIALS);
+            }
+        });
+
+        freePlayOneAdult = view.findViewById(R.id.free_play_one_adult_available);
+        freePlayOneAdult.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    unguidedFreePlay.add(UnguidedFreePlay.ONE_ADULT_IS_AVAILABLE);
+                else
+                    unguidedFreePlay.remove(UnguidedFreePlay.ONE_ADULT_IS_AVAILABLE);
+            }
+        });
+
         //Materials used checkboxes
         materialsScheduledUsedNo = view.findViewById(R.id.teching_materials_used_no);
         materialsScheduledUsedNo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                if (b)
+                if (b){
                     materialsScheduledUsedYes.setChecked(false);
+                    teachingLearningMaterialsUsed = false;
+                }
             }
         });
 
@@ -87,24 +214,108 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment {
         materialsScheduledUsedYes.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             @Override
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-                    if (b)
+                    if (b){
                         materialsScheduledUsedNo.setChecked(false);
+                        teachingLearningMaterialsUsed = true;
+                    }
+            }
+        });
+
+        //Activities took place
+        activityWelcome = view.findViewById(R.id.welcome_and_free_play);
+        activityWelcome.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b){
+                    activitiesTookPlace.add(Activities.WELCOME_AND_FREE_PLAY);
+                }else{
+                    activitiesTookPlace.remove(Activities.WELCOME_AND_FREE_PLAY);
+                }
+
+            }
+        });
+        activityOpeningSong = view.findViewById(R.id.opening_song);
+        activityOpeningSong.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    activitiesTookPlace.add(Activities.OPENING_SONG);
+                else
+                    activitiesTookPlace.remove(Activities.OPENING_SONG);
+            }
+        });
+
+        activityReview = view.findViewById(R.id.review_prevous_week);
+        activityReview.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    activitiesTookPlace.add(Activities.REVIEW_PREVIOUS_WEEK);
+                else
+                    activitiesTookPlace.remove(Activities.REVIEW_PREVIOUS_WEEK);
+            }
+        });
+
+        activityOne = view.findViewById(R.id.activity_1);
+        activityOne.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    activitiesTookPlace.add(Activities.ACTIVITY_1);
+                else
+                    activitiesTookPlace.remove(Activities.ACTIVITY_1);
+            }
+        });
+
+        activityTwo = view.findViewById(R.id.activity_2);
+        activityTwo.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    activitiesTookPlace.add(Activities.ACTIVITY_TWO);
+                else
+                    activitiesTookPlace.remove(Activities.ACTIVITY_TWO);
+            }
+        });
+
+        activityRecap = view.findViewById(R.id.recap_session);
+        activityRecap.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    activitiesTookPlace.add(Activities.RECAP_SESSION);
+                else
+                    activitiesTookPlace.remove(Activities.RECAP_SESSION);
+            }
+        });
+
+        activityClosingSong = view.findViewById(R.id.closing_song);
+        activityClosingSong.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+                if (b)
+                    activitiesTookPlace.add(Activities.CLOSING_SONG);
+                else
+                    activitiesTookPlace.remove(Activities.CLOSING_SONG);
             }
         });
 
         //Caregivers Encouraging Checkboxes
         encouragingCaregiversYesAllActivities = view.findViewById(R.id.yes_all_or_nearly_all);
-        encouragingCaregiversYesAllActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view, selectedOption -> caregiversEncouraging = selectedOption));
+        encouragingCaregiversYesAllActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view,
+                selectedOption -> caregiversEncouraging = selectedOption));
 
         encouragingCaregiversYesMostActivities = view.findViewById(R.id.yes_most);
-        encouragingCaregiversYesMostActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view, selectedOption -> caregiversEncouraging = selectedOption));
+        encouragingCaregiversYesMostActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view,
+                selectedOption -> caregiversEncouraging = selectedOption));
 
         encouragingCaregiversYesSomeActivities = view.findViewById(R.id.yes_some);
-        encouragingCaregiversYesSomeActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view, selectedOption -> caregiversEncouraging = selectedOption));
+        encouragingCaregiversYesSomeActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view,
+                selectedOption -> caregiversEncouraging = selectedOption));
 
         encouragingCaregiversNoneOrFew = view.findViewById(R.id.none_or_very_few);
-        encouragingCaregiversNoneOrFew.setOnCheckedChangeListener(new CaregiversEncouragingListener(view, selectedOption -> caregiversEncouraging = selectedOption));
-
+        encouragingCaregiversNoneOrFew.setOnCheckedChangeListener(new CaregiversEncouragingListener(view,
+                selectedOption -> caregiversEncouraging = selectedOption));
 
         //Brought Materials Checkboxes
         caregiversBroughtMaterialsYesAll = view.findViewById(R.id.materials_yes_all_or_nearly_all);
@@ -172,102 +383,31 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment {
 
     }
 
-    private static class CaregiversMaterialsListener implements CompoundButton.OnCheckedChangeListener {
-
-        View parentView;
-        CheckBox yesAll;
-        CheckBox yesMost;
-        CheckBox yesSome;
-        CheckBox none;
-
-        SelectedOption selectedOption;
-
-        CaregiversMaterialsListener(View parentView, SelectedOption so){
-            this.parentView = parentView;
-            this.selectedOption = so;
-            yesAll = (CheckBox) parentView.findViewById(R.id.materials_yes_all_or_nearly_all);
-            yesMost = (CheckBox) parentView.findViewById(R.id.materials_yes_most);
-            yesSome = (CheckBox) parentView.findViewById(R.id.materials_yes_some);
-            none = (CheckBox) parentView.findViewById(R.id.materials_none_or_very_few);
-        }
-
-        @Override
-        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-            if (b){
-                if (compoundButton.getId() == R.id.materials_yes_all_or_nearly_all){
-                    yesMost.setChecked(false);
-                    yesSome.setChecked(false);
-                    none.setChecked(false);
-                    selectedOption.onSelection("Yes all or Nearly all");
-                }else if (compoundButton.getId() == R.id.materials_yes_most){
-                    yesAll.setChecked(false);
-                    yesSome.setChecked(false);
-                    none.setChecked(false);
-                    selectedOption.onSelection("Yes most");
-                }else if (compoundButton.getId() == R.id.materials_yes_some){
-                    yesAll.setChecked(false);
-                    yesMost.setChecked(false);
-                    none.setChecked(false);
-                    selectedOption.onSelection("Yes some");
-                }else if (compoundButton.getId() == R.id.materials_none_or_very_few){
-                    yesAll.setChecked(false);
-                    yesMost.setChecked(false);
-                    yesSome.setChecked(false);
-                    selectedOption.onSelection("None or very few");
-                }
-            }
-        }
-    }
-
-    private static class CaregiversEncouragingListener implements CompoundButton.OnCheckedChangeListener {
-
-        View parentView;
-        CheckBox yesAll;
-        CheckBox yesMost;
-        CheckBox yesSome;
-        CheckBox none;
-        SelectedOption selectedOption;
-
-        CaregiversEncouragingListener(View parentView, SelectedOption selectedOption){
-            this.parentView = parentView;
-            this.selectedOption = selectedOption;
-            yesAll = (CheckBox) parentView.findViewById(R.id.yes_all_or_nearly_all);
-            yesMost = (CheckBox) parentView.findViewById(R.id.yes_most);
-            yesSome = (CheckBox) parentView.findViewById(R.id.yes_some);
-            none = (CheckBox) parentView.findViewById(R.id.none_or_very_few);
-        }
-
-        @Override
-        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-            if (b){
-                if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
-                    yesMost.setChecked(false);
-                    yesSome.setChecked(false);
-                    none.setChecked(false);
-                    selectedOption.onSelection("Yes all or nearly all");
-                }else if (compoundButton.getId() == R.id.yes_most){
-                    yesAll.setChecked(false);
-                    yesSome.setChecked(false);
-                    none.setChecked(false);
-                    selectedOption.onSelection("Yes most");
-                }else if (compoundButton.getId() == R.id.yes_some){
-                    yesAll.setChecked(false);
-                    yesMost.setChecked(false);
-                    none.setChecked(false);
-                    selectedOption.onSelection("Yes some");
-                }else if (compoundButton.getId() == R.id.none_or_very_few){
-                    yesAll.setChecked(false);
-                    yesMost.setChecked(false);
-                    yesSome.setChecked(false);
-                    selectedOption.onSelection("None or very few");
-                }
-            }
-        }
-    }
-
-    interface SelectedOption {
+    public interface SelectedOption {
         void onSelection(String selectedOption);
     }
 
+    enum Activities {
+        WELCOME_AND_FREE_PLAY,
+        OPENING_SONG,
+        REVIEW_PREVIOUS_WEEK,
+        ACTIVITY_1,
+        ACTIVITY_TWO,
+        RECAP_SESSION,
+        CLOSING_SONG
+    }
+
+    enum UnguidedFreePlay {
+        MOST_CHILDREN_ARE_PLAYING_WITH_MATERIALS,
+        ONE_ADULT_IS_AVAILABLE
+    }
+
+    enum CoveredTopics {
+        LANGUAGE,
+        COGNITIVE,
+        SOCIAL_EMOTIONAL,
+        CREATIVITY,
+        FORMAL_TEACHING
+    }
 
 }
