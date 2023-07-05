f34770c570138c30c4aee7efb50d00d3d775f1a5
issyzac <issyzac.iz@gmail.com>
2023-07-05 13:00:15 +0300

    :recycle: added constraints for selected answers

--- opensrp-chw/src/kk/java/org/smartregister/chw/fragment/GcFinalStepFragment.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/fragment/GcFinalStepFragment.java
@@ -1,5 +1,6 @@
 package org.smartregister.chw.fragment;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -15,25 +16,67 @@ import org.smartregister.chw.R;
 /**
  * Author issyzac on 04/07/2023
  */
-public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment {
+public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implements CompoundButton.OnCheckedChangeListener {
 
     LinearLayout difficultActivitiesList;
 
     CheckBox activitiesDifficultYes;
     CheckBox activitiesDifficultNo;
 
+    CheckBox encouragingCaregiversYesAllActivities;
+    CheckBox encouragingCaregiversYesMostActivities;
+    CheckBox encouragingCaregiversYesSomeActivities;
+    CheckBox encouragingCaregiversNoneOrFew;
+
+    CheckBox caregiversBroughtMaterialsYesAll;
+    CheckBox caregiversBroughtMaterialsYesMost;
+    CheckBox caregiversBroughtMaterialsYesSome;
+    CheckBox caregiversBroughtMaterialsNoneOrFew;
+
+    CaregiversMaterialsListener caregiversMaterialsListener;
+    CaregiversEncouragingListener caregiversEncouragingListener;
+
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View view = rootView;
-
         return rootView;
     }
 
     @Override
     public void setupViews(View view) {
         super.setupViews(view);
+
+        caregiversMaterialsListener = new CaregiversMaterialsListener(view);
+        caregiversEncouragingListener = new CaregiversEncouragingListener(view);
+
+        //Caregivers Encouraging Checkboxes
+        encouragingCaregiversYesAllActivities = view.findViewById(R.id.yes_all_or_nearly_all);
+        encouragingCaregiversYesAllActivities.setOnCheckedChangeListener(caregiversEncouragingListener);
+
+        encouragingCaregiversYesMostActivities = view.findViewById(R.id.yes_most);
+        encouragingCaregiversYesMostActivities.setOnCheckedChangeListener(caregiversEncouragingListener);
+
+        encouragingCaregiversYesSomeActivities = view.findViewById(R.id.yes_some);
+        encouragingCaregiversYesSomeActivities.setOnCheckedChangeListener(caregiversEncouragingListener);
+
+        encouragingCaregiversNoneOrFew = view.findViewById(R.id.none_or_very_few);
+        encouragingCaregiversNoneOrFew.setOnCheckedChangeListener(caregiversEncouragingListener);
+
+
+        //Brought Materials Checkboxes
+        caregiversBroughtMaterialsYesAll = view.findViewById(R.id.materials_yes_all_or_nearly_all);
+        caregiversBroughtMaterialsYesAll.setOnCheckedChangeListener(caregiversMaterialsListener);
+
+        caregiversBroughtMaterialsYesMost = view.findViewById(R.id.materials_yes_most);
+        caregiversBroughtMaterialsYesMost.setOnCheckedChangeListener(caregiversMaterialsListener);
+
+        caregiversBroughtMaterialsYesSome = view.findViewById(R.id.materials_yes_some);
+        caregiversBroughtMaterialsYesSome.setOnCheckedChangeListener(caregiversMaterialsListener);
+
+        caregiversBroughtMaterialsNoneOrFew = view.findViewById(R.id.materials_none_or_very_few);
+        caregiversBroughtMaterialsNoneOrFew.setOnCheckedChangeListener(caregiversMaterialsListener);
 
         difficultActivitiesList = view.findViewById(R.id.llDifficultActivitiesList);
         activitiesDifficultYes = view.findViewById(R.id.difficult_activities_yes);
@@ -73,4 +116,102 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment {
     public String getSessionDetails() {
         return null;
     }
+
+    @Override
+    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+        if (b){
+            if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
+
+            }else if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
+
+            }
+            else if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
+
+            }
+            else if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
+
+            }
+        }
+    }
+
+    private static class CaregiversMaterialsListener implements CompoundButton.OnCheckedChangeListener {
+
+        View parentView;
+        CheckBox yesAll;
+        CheckBox yesMost;
+        CheckBox yesSome;
+        CheckBox none;
+
+        CaregiversMaterialsListener(View parentView){
+            this.parentView = parentView;
+            yesAll = (CheckBox) parentView.findViewById(R.id.materials_yes_all_or_nearly_all);
+            yesMost = (CheckBox) parentView.findViewById(R.id.materials_yes_most);
+            yesSome = (CheckBox) parentView.findViewById(R.id.materials_yes_some);
+            none = (CheckBox) parentView.findViewById(R.id.materials_none_or_very_few);
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if (b){
+                if (compoundButton.getId() == R.id.materials_yes_all_or_nearly_all){
+                    yesMost.setChecked(false);
+                    yesSome.setChecked(false);
+                    none.setChecked(false);
+                }else if (compoundButton.getId() == R.id.materials_yes_most){
+                    yesAll.setChecked(false);
+                    yesSome.setChecked(false);
+                    none.setChecked(false);
+                }else if (compoundButton.getId() == R.id.materials_yes_some){
+                    yesAll.setChecked(false);
+                    yesMost.setChecked(false);
+                    none.setChecked(false);
+                }else if (compoundButton.getId() == R.id.materials_none_or_very_few){
+                    yesAll.setChecked(false);
+                    yesMost.setChecked(false);
+                    yesSome.setChecked(false);
+                }
+            }
+        }
+    }
+
+    private static class CaregiversEncouragingListener implements CompoundButton.OnCheckedChangeListener {
+
+        View parentView;
+        CheckBox yesAll;
+        CheckBox yesMost;
+        CheckBox yesSome;
+        CheckBox none;
+
+        CaregiversEncouragingListener(View parentView){
+            this.parentView = parentView;
+            yesAll = (CheckBox) parentView.findViewById(R.id.yes_all_or_nearly_all);
+            yesMost = (CheckBox) parentView.findViewById(R.id.yes_most);
+            yesSome = (CheckBox) parentView.findViewById(R.id.yes_some);
+            none = (CheckBox) parentView.findViewById(R.id.none_or_very_few);
+        }
+
+        @Override
+        public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+            if (b){
+                if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
+                    yesMost.setChecked(false);
+                    yesSome.setChecked(false);
+                    none.setChecked(false);
+                }else if (compoundButton.getId() == R.id.yes_most){
+                    yesAll.setChecked(false);
+                    yesSome.setChecked(false);
+                    none.setChecked(false);
+                }else if (compoundButton.getId() == R.id.yes_some){
+                    yesAll.setChecked(false);
+                    yesMost.setChecked(false);
+                    none.setChecked(false);
+                }else if (compoundButton.getId() == R.id.none_or_very_few){
+                    yesAll.setChecked(false);
+                    yesMost.setChecked(false);
+                    yesSome.setChecked(false);
+                }
+            }
+        }
+    }
+
 }
