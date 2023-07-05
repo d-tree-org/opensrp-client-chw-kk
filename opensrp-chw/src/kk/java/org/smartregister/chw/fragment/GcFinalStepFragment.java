f81c4fdce54aa19b4753acb6a949394b2d22ee82
issyzac <issyzac.iz@gmail.com>
2023-07-05 15:39:17 +0300

    :zap: added callback to listeners

--- opensrp-chw/src/kk/java/org/smartregister/chw/fragment/GcFinalStepFragment.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/fragment/GcFinalStepFragment.java
@@ -11,12 +11,15 @@ import android.widget.LinearLayout;
 
 import androidx.annotation.Nullable;
 
+import org.apache.commons.io.serialization.ValidatingObjectInputStream;
+import org.json.JSONObject;
 import org.smartregister.chw.R;
+import org.smartregister.chw.model.GroupSessionModel;
 
 /**
  * Author issyzac on 04/07/2023
  */
-public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implements CompoundButton.OnCheckedChangeListener {
+public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment {
 
     LinearLayout difficultActivitiesList;
 
@@ -36,8 +39,27 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
     CheckBox materialsScheduledUsedYes;
     CheckBox materialsScheduledUsedNo;
 
-    CaregiversMaterialsListener caregiversMaterialsListener;
-    CaregiversEncouragingListener caregiversEncouragingListener;
+    GroupSessionModel sessionModel;
+
+    private String activitiesTookPlace;
+    private boolean teachingLearningMaterialsUsed;
+    private String unguidedFreePlay;
+    private boolean anyDifficultActivities = false;
+    private String listOfDifficultActivities = "";
+    private String caregiversEncouraging;
+    private String caregiversBroughtMaterials;
+    private String topicsCovered;
+
+    private JSONObject sessionObject;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        //todo: to be changed to the same model started on previous steps
+        sessionModel = new GroupSessionModel();
+
+    }
 
     @Nullable
     @Override
@@ -50,9 +72,6 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
     @Override
     public void setupViews(View view) {
         super.setupViews(view);
-
-        caregiversMaterialsListener = new CaregiversMaterialsListener(view);
-        caregiversEncouragingListener = new CaregiversEncouragingListener(view);
 
         //Materials used checkboxes
         materialsScheduledUsedNo = view.findViewById(R.id.teching_materials_used_no);
@@ -75,30 +94,34 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
 
         //Caregivers Encouraging Checkboxes
         encouragingCaregiversYesAllActivities = view.findViewById(R.id.yes_all_or_nearly_all);
-        encouragingCaregiversYesAllActivities.setOnCheckedChangeListener(caregiversEncouragingListener);
+        encouragingCaregiversYesAllActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view, selectedOption -> caregiversEncouraging = selectedOption));
 
         encouragingCaregiversYesMostActivities = view.findViewById(R.id.yes_most);
-        encouragingCaregiversYesMostActivities.setOnCheckedChangeListener(caregiversEncouragingListener);
+        encouragingCaregiversYesMostActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view, selectedOption -> caregiversEncouraging = selectedOption));
 
         encouragingCaregiversYesSomeActivities = view.findViewById(R.id.yes_some);
-        encouragingCaregiversYesSomeActivities.setOnCheckedChangeListener(caregiversEncouragingListener);
+        encouragingCaregiversYesSomeActivities.setOnCheckedChangeListener(new CaregiversEncouragingListener(view, selectedOption -> caregiversEncouraging = selectedOption));
 
         encouragingCaregiversNoneOrFew = view.findViewById(R.id.none_or_very_few);
-        encouragingCaregiversNoneOrFew.setOnCheckedChangeListener(caregiversEncouragingListener);
+        encouragingCaregiversNoneOrFew.setOnCheckedChangeListener(new CaregiversEncouragingListener(view, selectedOption -> caregiversEncouraging = selectedOption));
 
 
         //Brought Materials Checkboxes
         caregiversBroughtMaterialsYesAll = view.findViewById(R.id.materials_yes_all_or_nearly_all);
-        caregiversBroughtMaterialsYesAll.setOnCheckedChangeListener(caregiversMaterialsListener);
+        caregiversBroughtMaterialsYesAll.setOnCheckedChangeListener(new CaregiversMaterialsListener(view,
+                selectedOption -> caregiversBroughtMaterials = selectedOption));
 
         caregiversBroughtMaterialsYesMost = view.findViewById(R.id.materials_yes_most);
-        caregiversBroughtMaterialsYesMost.setOnCheckedChangeListener(caregiversMaterialsListener);
+        caregiversBroughtMaterialsYesMost.setOnCheckedChangeListener(new CaregiversMaterialsListener(view,
+                selectedOption -> caregiversBroughtMaterials = selectedOption));
 
         caregiversBroughtMaterialsYesSome = view.findViewById(R.id.materials_yes_some);
-        caregiversBroughtMaterialsYesSome.setOnCheckedChangeListener(caregiversMaterialsListener);
+        caregiversBroughtMaterialsYesSome.setOnCheckedChangeListener(new CaregiversMaterialsListener(view,
+                selectedOption -> caregiversBroughtMaterials = selectedOption));
 
         caregiversBroughtMaterialsNoneOrFew = view.findViewById(R.id.materials_none_or_very_few);
-        caregiversBroughtMaterialsNoneOrFew.setOnCheckedChangeListener(caregiversMaterialsListener);
+        caregiversBroughtMaterialsNoneOrFew.setOnCheckedChangeListener(new CaregiversMaterialsListener(view,
+                selectedOption -> caregiversBroughtMaterials = selectedOption));
 
         difficultActivitiesList = view.findViewById(R.id.llDifficultActivitiesList);
         activitiesDifficultYes = view.findViewById(R.id.difficult_activities_yes);
@@ -109,6 +132,7 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (b){
                     activitiesDifficultYes.setChecked(false);
+                    anyDifficultActivities = false;
                 }
             }
         });
@@ -118,6 +142,7 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
             public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
                 if (b){
                     activitiesDifficultNo.setChecked(false);
+                    anyDifficultActivities = true;
                 }
                 difficultActivitiesLayoutController(b);
             }
@@ -139,21 +164,12 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
         return null;
     }
 
-    @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
-        if (b){
-            if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
+    private boolean validateFields(){
+        return false;
+    }
 
-            }else if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
+    private void createSessionObject(){
 
-            }
-            else if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
-
-            }
-            else if (compoundButton.getId() == R.id.yes_all_or_nearly_all){
-
-            }
-        }
     }
 
     private static class CaregiversMaterialsListener implements CompoundButton.OnCheckedChangeListener {
@@ -164,8 +180,11 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
         CheckBox yesSome;
         CheckBox none;
 
-        CaregiversMaterialsListener(View parentView){
+        SelectedOption selectedOption;
+
+        CaregiversMaterialsListener(View parentView, SelectedOption so){
             this.parentView = parentView;
+            this.selectedOption = so;
             yesAll = (CheckBox) parentView.findViewById(R.id.materials_yes_all_or_nearly_all);
             yesMost = (CheckBox) parentView.findViewById(R.id.materials_yes_most);
             yesSome = (CheckBox) parentView.findViewById(R.id.materials_yes_some);
@@ -179,18 +198,22 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
                     yesMost.setChecked(false);
                     yesSome.setChecked(false);
                     none.setChecked(false);
+                    selectedOption.onSelection("Yes all or Nearly all");
                 }else if (compoundButton.getId() == R.id.materials_yes_most){
                     yesAll.setChecked(false);
                     yesSome.setChecked(false);
                     none.setChecked(false);
+                    selectedOption.onSelection("Yes most");
                 }else if (compoundButton.getId() == R.id.materials_yes_some){
                     yesAll.setChecked(false);
                     yesMost.setChecked(false);
                     none.setChecked(false);
+                    selectedOption.onSelection("Yes some");
                 }else if (compoundButton.getId() == R.id.materials_none_or_very_few){
                     yesAll.setChecked(false);
                     yesMost.setChecked(false);
                     yesSome.setChecked(false);
+                    selectedOption.onSelection("None or very few");
                 }
             }
         }
@@ -203,9 +226,11 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
         CheckBox yesMost;
         CheckBox yesSome;
         CheckBox none;
+        SelectedOption selectedOption;
 
-        CaregiversEncouragingListener(View parentView){
+        CaregiversEncouragingListener(View parentView, SelectedOption selectedOption){
             this.parentView = parentView;
+            this.selectedOption = selectedOption;
             yesAll = (CheckBox) parentView.findViewById(R.id.yes_all_or_nearly_all);
             yesMost = (CheckBox) parentView.findViewById(R.id.yes_most);
             yesSome = (CheckBox) parentView.findViewById(R.id.yes_some);
@@ -219,21 +244,30 @@ public class GcFinalStepFragment extends BaseGroupSessionRegisterFragment implem
                     yesMost.setChecked(false);
                     yesSome.setChecked(false);
                     none.setChecked(false);
+                    selectedOption.onSelection("Yes all or nearly all");
                 }else if (compoundButton.getId() == R.id.yes_most){
                     yesAll.setChecked(false);
                     yesSome.setChecked(false);
                     none.setChecked(false);
+                    selectedOption.onSelection("Yes most");
                 }else if (compoundButton.getId() == R.id.yes_some){
                     yesAll.setChecked(false);
                     yesMost.setChecked(false);
                     none.setChecked(false);
+                    selectedOption.onSelection("Yes some");
                 }else if (compoundButton.getId() == R.id.none_or_very_few){
                     yesAll.setChecked(false);
                     yesMost.setChecked(false);
                     yesSome.setChecked(false);
+                    selectedOption.onSelection("None or very few");
                 }
             }
         }
     }
+
+    interface SelectedOption {
+        void onSelection(String selectedOption);
+    }
+
 
 }
