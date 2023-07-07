017c7d320dd2c4b2972986700758e6b2d547774b
issyzac <issyzac.iz@gmail.com>
2023-07-07 14:56:06 +0300

    :recycle: remove bottom navigation view

--- opensrp-chw/src/main/java/org/smartregister/chw/activity/GroupSessionRegisterActivity.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/activity/GroupSessionRegisterActivity.java
@@ -2,6 +2,7 @@ package org.smartregister.chw.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 import androidx.fragment.app.Fragment;
 
@@ -80,6 +81,7 @@ public class GroupSessionRegisterActivity extends BaseRegisterActivity implement
     protected void registerBottomNavigation() {
         bottomNavigationHelper = new BottomNavigationHelper();
         bottomNavigationView = findViewById(org.smartregister.R.id.bottom_navigation);
+        bottomNavigationView.setVisibility(View.GONE);
 
         if (bottomNavigationView != null) {
             bottomNavigationView.setLabelVisibilityMode(LabelVisibilityMode.LABEL_VISIBILITY_LABELED);
