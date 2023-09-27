e56e9faf872e8653ed5b96e3b0f50ad780803b4e
issyzac <issyzac.iz@gmail.com>
2023-09-27 16:41:51 +0300

    :construction: repository to clean edi id

---
+++ opensrp-chw/src/main/java/org/smartregister/chw/repository/EdiIdCleanUpRepository.java
@@ -0,0 +1,54 @@
+package org.smartregister.chw.repository;
+
+import android.annotation.SuppressLint;
+import android.database.Cursor;
+
+import org.smartregister.repository.BaseRepository;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import timber.log.Timber;
+
+/**
+ * Created by Kassim Sheghembe on 2023-09-27
+ */
+public class EdiIdCleanUpRepository extends BaseRepository {
+
+    private static final String BASE_ENTITY_ID = "baseEntityId";
+
+    public static final String CLIENTS_WITH_EDI_IDS_SQL = "WITH clients AS (" +
+            "SELECT " +
+            "baseEntityId, " +
+            "COALESCE(json_extract(json, '$.identifiers.edi_id'), json_extract(json, '$.identifiers.mother_edi_id')) as edi_id as edi_id " +
+            "FROM client " +
+            ")" +
+            "select * from clients where edi_id is not null";
+
+    public Map<String, String> clientsWithEdiIds() {
+
+        Map<String, String> ediIds = new HashMap<>();
+
+        Cursor cursor = null;
+
+        try {
+            cursor = getReadableDatabase().rawQuery(CLIENTS_WITH_EDI_IDS_SQL, new String[]{});
+
+            while (cursor.moveToNext()) {
+                @SuppressLint("Range") String baseEntityId = cursor.getString(cursor.getColumnIndex(BASE_ENTITY_ID));
+                @SuppressLint("Range") String ediId = cursor.getString(cursor.getColumnIndex("edi_id"));
+
+                ediIds.put(baseEntityId, ediId);
+            }
+
+        } catch (Exception e) {
+            Timber.e(e);
+        }
+        finally {
+            if (cursor != null && !cursor.isClosed())
+                cursor.close();
+        }
+
+        return ediIds;
+    }
+}
---
+++ opensrp-chw/src/main/java/org/smartregister/chw/repository/KKEventClientRepository.java
@@ -0,0 +1,114 @@
+package org.smartregister.chw.repository;
+
+import org.json.JSONObject;
+import org.smartregister.AllConstants;
+import org.smartregister.Context;
+import org.smartregister.chw.domain.CleanEDIStatus;
+import org.smartregister.domain.Event;
+import org.smartregister.domain.Client;
+import org.smartregister.domain.Obs;
+import org.smartregister.domain.db.EventClient;
+import org.smartregister.repository.BaseRepository;
+import org.smartregister.repository.EventClientRepository;
+import org.smartregister.view.activity.DrishtiApplication;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Map;
+
+import timber.log.Timber;
+
+/**
+ * Author issyzac on 27/09/2023
+ */
+public class KKEventClientRepository extends EventClientRepository {
+
+    public KKEventClientRepository(){}
+
+    public CleanEDIStatus removeEdiIdsFromClients() throws Exception {
+
+        String username = Context.getInstance().userService().getAllSharedPreferences().fetchRegisteredANM();
+
+        EdiIdCleanUpRepository ediIdCleanUpRepository = new EdiIdCleanUpRepository();
+
+        Map<String, String> clientsWithEdiIds = ediIdCleanUpRepository.clientsWithEdiIds();
+
+        if (clientsWithEdiIds.size() == 0)
+            return CleanEDIStatus.CLEANED;
+
+        //TODO: Change to Firebase events
+        Timber.e("%d duplicates for provider: %s - %s", clientsWithEdiIds.size(), username, ediIdCleanUpRepository.toString());
+
+        for (Map.Entry<String, String> clientWithEdiId : clientsWithEdiIds.entrySet()) {
+
+            String baseEntityId = clientWithEdiId.getKey();
+            String ediId = clientWithEdiId.getValue();
+
+            JSONObject clientJson = getClientByBaseEntityId(baseEntityId);
+            JSONObject identifiers = clientJson.getJSONObject(AllConstants.IDENTIFIERS);
+
+            String identifierLabel = identifiers.has("edi_id") ? "edi_id"
+                    : identifiers.has("mother_edi_id") ? "mother_edi_id" : "";
+
+            if (!identifierLabel.equals("")){
+                identifiers.remove(identifierLabel);
+                updateClientObject(clientJson, identifiers, baseEntityId, ediId);
+            }
+
+        }
+
+        return CleanEDIStatus.CLEANED;
+
+    }
+
+    private void updateClientObject(JSONObject clientJson, JSONObject identifiers, String baseEntityId, String ediID){
+        try {
+            clientJson.put(AllConstants.IDENTIFIERS, identifiers);
+
+            // Add events to process this
+            addorUpdateClient(baseEntityId, clientJson);
+
+            List<String> eventTypesList = new ArrayList<>();
+            eventTypesList.add("Family Member Registration");
+            eventTypesList.add("PNC Child Registration");
+
+            // fetch the registration event for the client (Child/Member)
+            List<EventClient> registrationEvent = getEvents(
+                    Collections.singletonList(baseEntityId),
+                    Collections.singletonList(BaseRepository.TYPE_Synced),
+                    eventTypesList
+            );
+
+            Event event = null;
+            if (!registrationEvent.isEmpty()){
+                event = registrationEvent.get(0).getEvent();
+            }
+
+            //Add EDI ID as observation to the registration event
+            Obs edi_id_obs = new Obs();
+            edi_id_obs.setFieldCode("edi_id");
+            edi_id_obs.setFieldType("concept");
+            edi_id_obs.setFieldDataType("text");
+            edi_id_obs.setParentCode("");
+            edi_id_obs.setValue(ediID);
+            edi_id_obs.setFormSubmissionField("edi_id");
+            edi_id_obs.setHumanReadableValue(ediID);
+
+            if (event != null)
+                event.addObs(edi_id_obs);
+
+            Client client = convert(clientJson, Client.class);
+
+            // reprocess the event
+            DrishtiApplication.getInstance().getClientProcessor().processClient(Collections.singletonList(new EventClient(event, client)));
+            markClientValidationStatus(baseEntityId, false);
+
+        }
+        catch (Exception e){
+            e.printStackTrace();
+        }
+
+    }
+
+}
