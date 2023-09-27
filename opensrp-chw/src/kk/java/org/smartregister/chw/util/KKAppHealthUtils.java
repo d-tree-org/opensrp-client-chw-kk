16436320969bf63247586c0448f6e5398efd0d4f
issyzac <issyzac.iz@gmail.com>
2023-09-27 16:40:47 +0300

    :zap: app health util methods

---
+++ opensrp-chw/src/kk/java/org/smartregister/chw/util/KKAppHealthUtils.java
@@ -0,0 +1,18 @@
+package org.smartregister.chw.util;
+
+import android.view.View;
+
+import org.smartregister.util.AppHealthUtils;
+
+/**
+ * Author issyzac on 27/09/2023
+ */
+public class KKAppHealthUtils extends AppHealthUtils {
+
+    public KKAppHealthUtils(View view) {
+        super(view);
+    }
+
+
+
+}
