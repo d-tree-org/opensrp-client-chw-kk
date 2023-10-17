297060403c00bc0e45b895b3dd637b36bf584530
maulidkategile <maulidkategile@gmail.com>
2023-10-17 16:48:12 +0300

    restore the encryption parameter

--- opensrp-chw/src/main/java/org/smartregister/chw/application/ChwSyncConfiguration.java
+++ opensrp-chw/src/main/java/org/smartregister/chw/application/ChwSyncConfiguration.java
@@ -59,7 +59,7 @@ public class ChwSyncConfiguration extends SyncConfiguration {
 
     @Override
     public SyncFilter getEncryptionParam() {
-        return SyncFilter.TEAM_ID;
+        return isProductionEnvironment() ? SyncFilter.PROVIDER : SyncFilter.LOCATION;
     }
 
 
