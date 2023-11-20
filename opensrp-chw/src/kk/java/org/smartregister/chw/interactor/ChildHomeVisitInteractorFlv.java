db0abb3d9ea24197739aa5f3e0e6e8022d712560
devcolonel <dev.marioanthony@gmail.com>
2023-11-20 22:19:47 +0300

    implement year ii child safety module evalution function

--- opensrp-chw/src/kk/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/interactor/ChildHomeVisitInteractorFlv.java
@@ -30,6 +30,7 @@ import org.smartregister.chw.helper.CCDCommunicationAssessmentAction;
 import org.smartregister.chw.helper.CCDDevelopmentScreeningAction;
 import org.smartregister.chw.helper.CCDIntroductionAction;
 import org.smartregister.chw.helper.ChildSafetyActionHelper;
+import org.smartregister.chw.helper.ChildSafetyActionHelperYearII;
 import org.smartregister.chw.helper.ComplimentaryFeedingActionHelper;
 import org.smartregister.chw.helper.ToddlerDangerSignAction;
 import org.smartregister.chw.util.BangoKititaPages;
@@ -180,8 +181,8 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
         evaluateCCDDevelopmentScreening(serviceWrapperMap);
     }
 
-    private void evaluateYearIIModules() {
-
+    private void evaluateYearIIModules() throws Exception {
+        evaluateCCDChildSafetyYearII();
     }
 
     private void evaluateVisitLocation() throws BaseAncHomeVisitAction.ValidationException {
@@ -796,6 +797,27 @@ public class ChildHomeVisitInteractorFlv extends DefaultChildHomeVisitInteractor
                 .build();
 
         actionList.put(title, malnutritionScreeningAction);
+    }
+
+    private void evaluateCCDChildSafetyYearII() throws Exception {
+
+        String title = context.getString(R.string.child_safety_year_ii);
+
+        ChildSafetyActionHelperYearII childSafetyActionHelperYearII = new ChildSafetyActionHelperYearII();
+
+        Map<String, List<VisitDetail>> details = getDetails(KKCoreConstants.ChildVisitEvents.CHILD_SAFETY);
+
+        BaseAncHomeVisitAction child_safety_action = new BaseAncHomeVisitAction.Builder(context, title)
+                .withOptional(false)
+                .withDetails(details)
+                .withFormName("child_hv_child_safety_year_ii")
+                .withPayloadType(BaseAncHomeVisitAction.PayloadType.SERVICE)
+                .withProcessingMode(BaseAncHomeVisitAction.ProcessingMode.COMBINED)
+                .withHelper(childSafetyActionHelperYearII)
+                .build();
+
+        actionList.put(title, child_safety_action);
+
     }
 
     private String getBreastfeedingServiceTittle(String serviceName) {
