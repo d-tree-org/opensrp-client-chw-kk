98e46209505b006d57be80e431da4fe664897321
issyzac <issyzac.iz@gmail.com>
2023-12-14 10:21:43 +0300

    :bucket: cleanup file name

--- opensrp-chw/src/kk/java/org/smartregister/chw/actionhelper/BreastfeedingHelperYearII.java
+++ opensrp-chw/src/kk/java/org/smartregister/chw/actionhelper/BreastfeedingHelperYearII.java
@@ -4,33 +4,71 @@ import static org.smartregister.chw.util.ChildVisitUtil.getTranslatedOtherFood;
 
 import android.content.Context;
 
+import com.vijay.jsonwizard.constants.JsonFormConstants;
+
 import org.apache.commons.lang3.StringUtils;
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.smartregister.chw.R;
 import org.smartregister.chw.anc.actionhelper.HomeVisitActionHelper;
 import org.smartregister.chw.anc.model.BaseAncHomeVisitAction;
+import org.smartregister.chw.anc.util.JsonFormUtils;
 
 import java.text.MessageFormat;
+import java.util.Objects;
 
 import timber.log.Timber;
 
 public class NewBornCareBreastfeedingHelperYearII  extends HomeVisitActionHelper {
     private final Context context;
 
+    private String jsonString;
+    private int childAgeInMonths;
     private String breastfeed_current;
-
     private String other_food_child_feeds;
+    private String important_feeding_child;
+    private String breastfeed_progress;
 
-    public NewBornCareBreastfeedingHelperYearII(Context context) {
+    public NewBornCareBreastfeedingHelperYearII(Context context, int childAge) {
+        this.childAgeInMonths = childAge;
         this.context = context;
     }
 
     @Override
+    public String getPreProcessed() {
+
+        try {
+
+            JSONObject jsonObject = new JSONObject(jsonString);
+            JSONArray fields = JsonFormUtils.fields(jsonObject);
+
+            //15, 18, 21, 24
+            if (childAgeInMonths == 15 || childAgeInMonths == 18 ||
+                childAgeInMonths == 21 || childAgeInMonths == 24){
+                Objects.requireNonNull(JsonFormUtils.getFieldJSONObject(fields, "other_food_child_feeds")).put(JsonFormConstants.HIDDEN, true);
+                Objects.requireNonNull(JsonFormUtils.getFieldJSONObject(fields, "important_feeding_child")).put(JsonFormConstants.HIDDEN, true);
+                Objects.requireNonNull(JsonFormUtils.getFieldJSONObject(fields, "breastfeed_progress")).put(JsonFormConstants.HIDDEN, true);
+            }
+
+            return jsonObject.toString();
+
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return super.getPreProcessed();
+    }
+
+    @Override
     public void onPayloadReceived(String jsonPayload) {
+
+        this.jsonString = jsonPayload;
+
         try {
             JSONObject jsonObject = new JSONObject(jsonPayload);
             breastfeed_current = org.smartregister.chw.util.JsonFormUtils.getValue(jsonObject, "breastfeed_current");
             other_food_child_feeds = org.smartregister.chw.util.JsonFormUtils.getValue(jsonObject, "other_food_child_feeds");
+            important_feeding_child = org.smartregister.chw.util.JsonFormUtils.getValue(jsonObject, "important_feeding_child");
+            breastfeed_progress = org.smartregister.chw.util.JsonFormUtils.getValue(jsonObject, "breastfeed_progress");
         } catch (Exception e) {
             Timber.e(e);
         }
